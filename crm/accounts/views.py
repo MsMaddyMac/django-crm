0a65270e390456baa2d73bd110ffe9d457fe1de3
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-10-30 17:31:12 -0400

    creates dashboard.html template and updates home view

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views (pages) here.
+# this is a view which will show the string passed into the HttpResponse() when someone visits that particular page(view)
+# def home(request):
+#   return HttpResponse('home')
+
+# this is what the def changes to if you have a template to render on that page(view)
+# you point it to the file path of the template within the templates directory. In this case within the templates directory there is an accounts directory and within that a dashboard.html file
 def home(request):
-  return HttpResponse('home')
+  return render(request, 'accounts/dashboard.html')
 
 def products(request):
   return HttpResponse('products')
