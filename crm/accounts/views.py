a4061b5840a4fb7d02488ce37b70a0489898b450
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-10-30 17:12:48 -0400

    creates accounts url.py and three views

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views (pages) here.
+def home(request):
+  return HttpResponse('home')
+
+def products(request):
+  return HttpResponse('products')
+
+def customer(request):
+  return HttpResponse('customer')
+
+# once you create your views import them into the urls.py file you created in this same directory
+
