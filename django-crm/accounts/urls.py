3b755fc54485bd426bbc5df0140945f006fa27cd
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-18 22:06:10 -0500

    adds dynamic links in navbar and customer view

--- django-crm/accounts/urls.py
+++ django-crm/accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<str:pk>/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
 ]
 
 # once you create paths for views include them in the urls.py of the main app 'crm app'
