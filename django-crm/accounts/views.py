6a175093a013601178118b7cc4966b87c23ba231
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-19 15:04:27 -0500

    adds flash message to login page

--- django-crm/accounts/views.py
+++ django-crm/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+# flash message
+from django.contrib import messages
 
 from .models import *
 from .forms import *
@@ -21,6 +23,10 @@ def registerPage(request):
     form = CreateUserForm(request.POST)
     if form.is_valid():
       form.save()
+      user = form.cleaned_data.get('username')
+      messages.success(request, 'Account was created for ' + user)
+      
+      return redirect('login')
       
   context = {'form': form}
   return render(request, 'accounts/register.html', context)
