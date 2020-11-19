557d0c91a1f6115313b380e9b11f1f70d5bde830
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-18 22:47:31 -0500

    adds delete order functionality

--- django-crm/accounts/views.py
+++ django-crm/accounts/views.py
@@ -62,5 +62,14 @@ def updateOrder(request, pk):
   context = {'form': form}
   return render(request, 'accounts/order_form.html', context)
 
+def deleteOrder(request, pk):
+  order = Order.objects.get(id=pk)
+  if request.method == 'POST':
+    order.delete()
+    return redirect('/')
+  
+  context = {'order': order}
+  return render(request, 'accounts/delete.html', context)
+
 # once you create your views import them into the urls.py file you created in this same directory
 
