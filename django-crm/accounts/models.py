94544236a1904699b54cd93dd30a0ef52b79d640
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-18 22:35:56 -0500

    creates update functionality for order

--- django-crm/accounts/models.py
+++ django-crm/accounts/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
 class Order(models.Model):
   STATUS = (
     ('Pending', 'Pending'),
-    ('Out for deliver', 'Out for deliver'),
+    ('Out for delivery', 'Out for delivery'),
     ('Delivered', 'Delivered'),
   )
   # One-to-many relationship 
--- django-crm/accounts/views.py
+++ django-crm/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
@@ -39,6 +39,26 @@ def customer(request, pk):
 
 def createOrder(request):
   form = OrderForm()
+  if request.method == 'POST':
+    # print('printing POST:', request.POST)
+    form = OrderForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
+    
+  context = {'form': form}
+  return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+  order = Order.objects.get(id=pk)
+  form = OrderForm(instance=order)
+  
+  if request.method == 'POST':
+    form = OrderForm(request.POST, instance=order)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
+    
   context = {'form': form}
   return render(request, 'accounts/order_form.html', context)
 
