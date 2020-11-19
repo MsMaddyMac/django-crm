8a6ae40b99fabbbe268e6f812c078ed7ecc8cb65
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-18 21:52:35 -0500

    queries customer orders and info renders in customer.html

--- django-crm/accounts/urls.py
+++ django-crm/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
-    path('customer/', views.customer),
+    path('customer/<str:pk>/', views.customer),
 ]
 
 # once you create paths for views include them in the urls.py of the main app 'crm app'
--- django-crm/accounts/views.py
+++ django-crm/accounts/views.py
@@ -12,9 +12,14 @@ from .models import *
 def home(request):
   orders = Order.objects.all()
   customers = Customer.objects.all()
-  print('customers', customers)
   
-  context = {'orders': orders, 'customers': customers}
+  total_customers = customers.count()
+  
+  total_orders = orders.count()
+  delivered = orders.filter(status='Delivered').count()
+  pending = orders.filter(status='Pending').count()
+  
+  context = {'orders': orders, 'customers': customers, 'total_customers': total_customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
   
   return render(request, 'accounts/dashboard.html', context)
 
@@ -22,8 +27,13 @@ def products(request):
   products = Product.objects.all()
   return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-  return render(request, 'accounts/customer.html')
+def customer(request, pk):
+  customer = Customer.objects.get(id=pk)
+  
+  orders = customer.order_set.all()
+  
+  context = {'customer': customer, 'orders': orders}
+  return render(request, 'accounts/customer.html', context)
 
 # once you create your views import them into the urls.py file you created in this same directory
 
