d119f7e9a208346dc11e9f616ea3ca2719a46e14
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-11-19 20:36:06 -0500

    creates signals to automatically create customer profile with new user

--- django-crm/accounts/views.py
+++ django-crm/accounts/views.py
@@ -32,9 +32,9 @@ def registerPage(request):
       user = form.save()
       username = form.cleaned_data.get('username')
       # creates a user inside the customer group
-      group = Group.objects.get(name='customer')
-      user.groups.add(group)
-      Customer.objects.create(user=user)
+      # group = Group.objects.get(name='customer')
+      # user.groups.add(group)
+      # Customer.objects.create(user=user)
       
       messages.success(request, 'Account was created for ' + username)
       
@@ -95,7 +95,16 @@ def userPage(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['customer'])
 def accountSettings(request):
-  context = {}
+  customer = request.user.customer
+  form = CustomerForm(instance=customer)
+  
+  if request.method == 'POST':
+      form = CustomerForm(request.POST, request.FILES, instance=customer)
+      if form.is_valid():
+        form.save()
+
+    
+  context = {'form': form}
   return render(request, 'accounts/account_settings.html', context)
 
 @login_required(login_url='login')
